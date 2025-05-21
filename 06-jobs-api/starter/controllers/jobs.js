4c46af025debbea8e84b81a00f877a8196e3675b
Edward Marquette Wilhite <quettehanson@gmail.com>
2025-05-20 22:39:39 -0400

    week10

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -3,11 +3,27 @@ const {BadRequestError, NotFoundError} = require('../errors')
 const Job = require('../models/Job')
 
 const getAllJobs = async (req, res) => {
-    res.send('Get all jobs')
+    const jobs = await Job.find({createdBy:req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({jobs, count: jobs.length})
 }
 
 const getJob = async (req, res) => {
-    res.send('Get job')
+    const { user:
+                {userId},
+            params:
+                {id:jobId}} = req
+
+    const job = await Job.findOne({
+        _id:jobId,createdBy:userId
+    })
+
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+
+    }
+
+    res.status(StatusCodes.OK).json({ job })
+
 }
 
 const createJobs = async (req, res) => {
@@ -18,11 +34,46 @@ const createJobs = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-    res.send('Update job')
+
+    const { 
+        body: {company,position},
+        user: { userId },
+        params: { id: jobId }
+    } = req
+
+    if (company === '' || position === '' ){
+        throw new BadRequestError('Company or position fields cannot be empty')
+
+    }
+    const job = await Job.findByIdAndUpdate({_id:jobId,createdBy:userId}, req.body, {new:true, runValidators:true})
+
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+
+    }
+
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const deleteJob = async (req, res) => {
-    res.send('delete job')
+    const { 
+        body: {company,position},
+        user: { userId },
+        params: { id: jobId }
+    } = req
+
+    const job = await Job.findOneAndRemove({
+        _id:jobId,
+        createdBy:userId
+    })
+
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+
+    }
+
+    res.status(StatusCodes.OK).send()
+
 }
 
 
